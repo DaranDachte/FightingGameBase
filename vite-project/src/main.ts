c56a4d2deddb952d068c83d32b82c77202c487de
Daran Dachte <darandachte@gmail.com>
2023-12-09 18:46:47 +0100

    draw the game

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -16,5 +16,77 @@ canvas.height = HEIGHT;
 //Декларируем контекст
 const ctx = canvas.getContext("2d")!;
 
-ctx.fillStyle = "rgb(0,0,0)";
-ctx.fillRect(START_POINT.x, START_POINT.y, WIDTH, HEIGHT);
+type Position = {
+  x: number;
+  y: number;
+};
+
+type Velocity = {
+  x: number;
+  y: number;
+};
+
+type PlayerConfig = {
+  position: Position;
+  color: string;
+  velocity: Velocity;
+};
+
+class Player {
+  position: Position;
+  velocity: Velocity;
+  color: string;
+  width: number = 60;
+  heigth: number = 150;
+  constructor({ position, color, velocity }: PlayerConfig) {
+    this.position = position;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.heigth);
+  }
+  update() {
+    this.draw();
+    this.position.y = this.position.y + this.velocity.y;
+    if (this.position.y + this.heigth === HEIGHT) {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player1 = new Player({
+  color: "red",
+  position: {
+    x: 50,
+    y: 50,
+  },
+  velocity: {
+    x: 0,
+    y: 5,
+  },
+});
+
+const player2 = new Player({
+  color: "blue",
+  position: {
+    x: 450,
+    y: 50,
+  },
+  velocity: {
+    x: 0,
+    y: 1,
+  },
+});
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = "rgb(0,0,0)";
+  ctx.fillRect(START_POINT.x, START_POINT.y, WIDTH, HEIGHT);
+  player1.update();
+  player2.update();
+};
+
+animate();
